dec395340c8def7ef016164a7fcb4fdd76c055e4
Habib Saker Hernandez <habsak@gmail.com>
2022-06-26 19:17:29 -0500

    Refactor jaden_casing_strings.js

--- jaden_casing_strings.js
+++ jaden_casing_strings.js
@@ -1,7 +1,7 @@
 String.prototype.toJadenCase = function () {
   return this
             .split(" ")
-            .map( word => word = word[0].toUpperCase() + word.slice(1, word.length) )
+            .map( word => word = word[0].toUpperCase() + word.slice(1) )
             .join(" ");
 }
 
